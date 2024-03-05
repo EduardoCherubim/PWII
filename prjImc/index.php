cd95fccb99b0831ac8dcf17c76d23bf6d2c53653
EduardoCherubim <142914224+EduardoCherubim@users.noreply.github.com>
2024-03-05 12:12:09 -0300

    Add files via upload

---
+++ prjImc/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IMC</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php 
+    echo 
+    "<table border='1' class=''>
+        <tr>
+            <td>MENOR QUE 18,5</td>
+            <td>MAGREZA</td>
+        </tr>
+        <tr>
+            <td>ENTRE 18,5 E 24,9</td>
+            <td>NORMAL</td>
+        </tr>
+        <tr>
+            <td>ENTRE 25,0 E 29,9</td>
+            <td>SOBREPESO</td>
+        </tr>
+        <tr>
+            <td>ENTRE 30,0 E 39,9</td>
+            <td>OBESIDADE</td>
+        </tr>
+        <tr>
+            <td>MAIOR QUE 40,0</td>
+            <td>OBESIDADE GRAVE</td>
+        </tr>
+    </table>"
+    ?>
+</body>
+</html>
