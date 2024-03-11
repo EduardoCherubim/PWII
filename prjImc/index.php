5777dbcbf0a2ecdb4e37f5da830baed67cb894f4
EduardoCherubim <142914224+EduardoCherubim@users.noreply.github.com>
2024-03-11 19:43:25 -0300

    Update index.php

--- prjImc/index.php
+++ prjImc/index.php
@@ -8,29 +8,41 @@
 </head>
 <body>
     <?php 
-    echo 
-    "<table border='1'>
-        <tr>
-            <td>MENOR QUE 18,5</td>
-            <td>MAGREZA</td>
-        </tr>
-        <tr>
-            <td>ENTRE 18,5 E 24,9</td>
-            <td>NORMAL</td>
-        </tr>
-        <tr>
-            <td>ENTRE 25,0 E 29,9</td>
-            <td>SOBREPESO</td>
-        </tr>
-        <tr>
-            <td>ENTRE 30,0 E 39,9</td>
-            <td>OBESIDADE</td>
-        </tr>
-        <tr>
-            <td>MAIOR QUE 40,0</td>
-            <td>OBESIDADE GRAVE</td>
-        </tr>
-    </table>"
+    echo
+    "<div id='container'>
+        <table border='1'>
+            <div class='linha1'>
+            <tr>
+                <td>MENOR QUE 18,5</td>
+                <td>MAGREZA</td>
+            </tr>
+            </div>
+            <div class='linha2'>
+            <tr>
+                <td>ENTRE 18,5 E 24,9</td>
+                <td>NORMAL</td>
+            </tr>
+            </div>
+            <div class='linha1'>
+            <tr>
+                <td>ENTRE 25,0 E 29,9</td>
+                <td>SOBREPESO</td>
+            </tr>
+            </div>
+            <div class='linha2'>
+            <tr>
+                <td>ENTRE 30,0 E 39,9</td>
+                <td>OBESIDADE</td>
+            </tr>
+            </div>
+            <div class='linha1'>
+            <tr>
+                <td>MAIOR QUE 40,0</td>
+                <td>OBESIDADE GRAVE</td>
+            </tr>
+            </div>
+        </table>
+    </div>"
     ?>
 </body>
 </html>
