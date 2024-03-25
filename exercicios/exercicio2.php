137835ef84759ff252cd19c54995042a761cffbf
EduardoCherubim <142914224+EduardoCherubim@users.noreply.github.com>
2024-03-25 11:57:45 -0300

    Update exercicio2.php

--- exercicios/exercicio2.php
+++ exercicios/exercicio2.php
@@ -1,9 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercicio2</title>
+    <style>
+        #pares{
+            background-color: blue;
+            color: white;
+            width: 100px;
+        }
+        #impares{
+            background-color: black;
+            color: red;
+            width: 100px;
+        }
+    </style>
 </head>
 <body>
     <?php 
