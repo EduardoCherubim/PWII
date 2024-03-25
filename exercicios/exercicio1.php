b4363413b9d16083e9bf0997e491a6b8a5286f82
EduardoCherubim <142914224+EduardoCherubim@users.noreply.github.com>
2024-03-25 11:57:26 -0300

    Update exercicio1.php

--- exercicios/exercicio1.php
+++ exercicios/exercicio1.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,13 +8,42 @@
     <title>Exercicio1</title>
 </head>
 <body>
+    <h1>Exercicio1</h1>
+    <br>
+    <h6>Triangulo-1</h6>
+    <h6>Quadrado-2</h6>
+    <h6>Circulo-3</h6>
+    <h6>Losango-4</h6>
     <?php
+
+
+
+    $v=4;
     
-    if($v=1){
-      echo "<img src="
+    if($v==1){
+      echo "<br> <p>Triangulo</p> <img src='image/transferir.png'";
+    }
+    
+
+    else if($v==2){
+    echo "<br> <p>Quadrado</p> <img src='image/images.png'";
     }
 
+   else if($v==3){
+    echo "<br> <p>Circulo</p> <img src='image/NOOSSA.png'";
+   }
+
+   else if($v==4){
+    echo "<br> <p>Losango</p> <img src='image/losan.png'";
+   }
+
+   else{
+    echo "Valor inválido";
+   }
+
     ?>
+
+    <a href="./index.php">Voltar</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
