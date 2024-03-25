81e50547b7fcec010a325960853fe04be4cbc1f6
EduardoCherubim <142914224+EduardoCherubim@users.noreply.github.com>
2024-03-25 11:58:11 -0300

    Add files via upload

---
+++ exercicios/exercicio3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 3</title>
+</head>
+<body>
+    <?php
+        for($i=1;$i <=10;$i++){
+            for($j=1;$j <=10;$j++){
+                echo "<p>".$i * $j."</p>";
+            }
+            echo "<br>";
+        }
+    ?>
+</body>
+</html>
