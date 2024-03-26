aef7a5146269abea290d08867807c292b446afc5
EduardoCherubim <142914224+EduardoCherubim@users.noreply.github.com>
2024-03-26 12:07:44 -0300

    Add files via upload

---
+++ exercicios2/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cliente</title>
+</head>
+<body>
+
+    <?php include('navbar.php')?>
+   <h1>Exercício 8</h1>
+    
+   <section>
+    <label>Nome</label>
+   </section>
+   <?php
+
+   ?>
+
+    <?php include('footer.php')?>
+
+</body>
+</html>
