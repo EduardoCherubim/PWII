9e9ea5668ac2fdbe87d0f62d6edc6b2bad673fbd
EduardoCherubim <142914224+EduardoCherubim@users.noreply.github.com>
2024-06-17 15:37:22 -0300

    Delete SiteEtec directory

--- SiteEtec/footer.php
+++
@@ -1,9 +0,0 @@
-<style>
-  .rodape{
-    margin-top: 10%;
-  }
-</style>
-<footer width="120%" class="rodape">
-  <div  class="text-bg-dark p-3">&copy; Todos os direitos reservados<br> Etec de Guaianazes -  R. gaspar dias de ataide, 290 - Guaianases, São Paulo</div>
-</footer>
-  
